896ba224d99aee4dae4ccb1316a42083d501a1a3
Juri <jur@pospelow.de>
2024-01-09 07:43:58 +0100

    netlog: use Device class

--- netlog.cpp
+++ netlog.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "Device.hpp"
+
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/ini_parser.hpp>
 
@@ -20,7 +22,11 @@ int main(int argc, char** argv)
     try
     {
         read_ini(fileName, config);
-        cout << config.get<std::string>("net1.pc1_name") << ": " << config.get<std::string>("net1.pc1_addr") <<endl;
+        // cout << config.get<std::string>("net1.pc1_name") << ": " << config.get<std::string>("net1.pc1_addr") <<endl;
+        Device dev1(config.get<std::string>("net1.pc1_name"), config.get<std::string>("net1.pc1_addr"));
+        cout << dev1.name() << ": " << dev1.ip() << endl;
+        Device dev2(config.get<std::string>("net1.pc2_name"), config.get<std::string>("net1.pc2_addr"));
+        cout << dev2.name() << ": " << dev2.ip() << endl;
     }
     catch (boost::property_tree::ini_parser_error& error)
     {
