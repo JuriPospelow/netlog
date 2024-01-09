43bbb439eeaf83569c461017ba02b3b453670758
Juri <jur@pospelow.de>
2024-01-09 08:28:40 +0100

    netlog: use Net class

--- netlog.cpp
+++ netlog.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
-#include "Device.hpp"
-
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/ini_parser.hpp>
+
+#include "Device.hpp"
+#include "Net.hpp"
 
 using namespace std;
 
@@ -19,21 +20,19 @@ int main(int argc, char** argv)
     cout << "read " << fileName << "\n";
 
     boost::property_tree::ptree config;
+
     vector<Device> v_dev;
     int cnt{};
+
+    Net nr1("n1");
 
     try
     {
         read_ini(fileName, config);
-        // cout << config.get<std::string>("net1.pc1_name") << ": " << config.get<std::string>("net1.pc1_addr") <<endl;
-        Device dev1(config.get<std::string>("net1.pc1_name"), config.get<std::string>("net1.pc1_addr"));
-        cout << dev1.name() << ": " << dev1.ip() << endl;
-        Device dev2(config.get<std::string>("net1.pc2_name"), config.get<std::string>("net1.pc2_addr"));
-        cout << dev2.name() << ": " << dev2.ip() << endl;
-
-        // string name {"net1.pc1_"};
-        // v_dev.push_back({config.get<std::string>(name+"name"), config.get<std::string>("net1.pc1_addr")});
-        // v_dev.push_back(dev2);
+        // Device dev1(config.get<std::string>("net1.pc1_name"), config.get<std::string>("net1.pc1_addr"));
+        // cout << dev1.name() << ": " << dev1.ip() << endl;
+        // Device dev2(config.get<std::string>("net1.pc2_name"), config.get<std::string>("net1.pc2_addr"));
+        // cout << dev2.name() << ": " << dev2.ip() << endl;
 
         cnt = config.get<int>("net1.number");
         string name_prefix {"net1.pc"};
@@ -41,6 +40,8 @@ int main(int argc, char** argv)
         for(int i{0}; i < cnt; ++i) {
             name_prefix += to_string(i+1);
             v_dev.push_back({config.get<std::string>(name_prefix+"_name"), config.get<std::string>(name_prefix+"_addr")});
+            nr1.devices.push_back({config.get<std::string>(name_prefix+"_name"), config.get<std::string>(name_prefix+"_addr")});
+
             name_prefix = "net1.pc";
         }
     }
@@ -54,6 +55,10 @@ int main(int argc, char** argv)
 
     for(int i{0}; i < cnt; ++i) {
         cout << v_dev[i].name() << ": " << v_dev[i].ip() << endl;
+    }
+
+    for(int i{0}; i < cnt; ++i) {
+        cout << nr1.devices[i].name() << ": " << nr1.devices[i].ip() << endl;
     }
 
 
