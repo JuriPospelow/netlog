875bd0d2fc7912b6f79c8a5f45396dff63d3ef43
Juri <jur@pospelow.de>
2024-01-11 08:31:43 +0100

    netlog: introduce create_net()

--- netlog.cpp
+++ netlog.cpp
@@ -8,6 +8,20 @@
 
 using namespace std;
 
+    boost::property_tree::ptree config;
+
+Net create_net(int cnt_dev, const string& tmp_name)
+{
+    Net tmp_net("tmp_net");
+    for(int i{0}; i < cnt_dev; ++i) {
+        string name_prefix = tmp_name + to_string(i+1);
+        tmp_net.devices.push_back({config.get<std::string>(name_prefix+"_name"), config.get<std::string>(name_prefix+"_addr")});
+
+        name_prefix = tmp_name;
+    }
+    return tmp_net;
+}
+
 int main(int argc, char** argv)
 {
     std::string fileName {"dummy.ini"};
@@ -19,56 +33,26 @@ int main(int argc, char** argv)
 
     cout << "read " << fileName << "\n";
 
-    boost::property_tree::ptree config;
-
-    int cnt{};
-
+    int cnt_dev{};
+    int cnt_net{};
     vector<Net> nets{};
-
-    // Net nr1("n1");
-    // Net nr2("n2");
 
     try
     {
         read_ini(fileName, config);
 
-        for (int j{}; j < config.get<int>("nets.number"); ++j) {
+        cnt_net = config.get<int>("nets.number");
+
+        for (int j{}; j < cnt_net; ++j) {
             string net_name{"net"};
             net_name += to_string(j+1);
 
-            Net tmp_net("tmp_net");
+            cnt_dev = config.get<int>(net_name+".number");
+            // string tmp_name {net_name+".pc"};
 
-            cnt = config.get<int>(net_name+".number");
-            string name_prefix {net_name+".pc"};
-
-            for(int i{0}; i < cnt; ++i) {
-                name_prefix += to_string(i+1);
-                tmp_net.devices.push_back({config.get<std::string>(name_prefix+"_name"), config.get<std::string>(name_prefix+"_addr")});
-
-                name_prefix = net_name +".pc";
-            }
-
-            nets.push_back(tmp_net);
+            nets.push_back(create_net(cnt_dev, net_name+".pc"));
             net_name = "net";
-            tmp_net.devices.clear();
         }
-
-        // net_name = "net";
-        // net_name += to_string(2);
-
-        // cnt = config.get<int>(net_name+".number");
-        // name_prefix = net_name+".pc";
-
-        // for(int i{0}; i < cnt; ++i) {
-            // name_prefix += to_string(i+1);
-            // nr2.devices.push_back({config.get<std::string>(name_prefix+"_name"), config.get<std::string>(name_prefix+"_addr")});
-
-            // name_prefix = net_name +".pc";
-        // }
-
-        // nets.push_back(nr2);
-
-
     }
     catch (boost::property_tree::ini_parser_error& error)
     {
@@ -78,8 +62,8 @@ int main(int argc, char** argv)
             << error.line() << std::endl;
     }
 #if 1
-    for(int j{0}; j < 2; ++j) {
-        for(int i{0}; i < cnt; ++i) {
+    for(int j{0}; j < cnt_net; ++j) {
+        for(int i{0}; i < cnt_dev; ++i) {
             cout << nets[j].devices[i].name() << ": " << nets[j].devices[i].ip() << endl;
         }
     }
