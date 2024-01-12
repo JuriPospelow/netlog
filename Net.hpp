c247c10f3bfb15da804e4dee7d64078627410614
Juri <jur@pospelow.de>
2024-01-12 08:43:14 +0100

    netlog: Net: introduce cnt_devs

--- Net.hpp
+++ Net.hpp
@@ -9,8 +9,12 @@ public:
     Net(std::string_view n)
     : name {n} {}
     void readStatus(std::string_view);
+    void set_cnt_devs(int cnt_dev) { cnt_devs = cnt_dev; }
+    int get_cnt_devs() { return cnt_devs; }
 
     std::string name{};
     std::vector <Device> devices{};
     std::string net_map{};
+
+    int cnt_devs{};
 };
