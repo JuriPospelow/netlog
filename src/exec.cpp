d4c0be52dc259794ee12b2f13e16151d9e182aef
Juri <jur@pospelow.de>
2024-01-16 20:42:10 +0100

    netlog: exec: fix terminated program

--- src/exec.cpp
+++ src/exec.cpp
@@ -5,15 +5,19 @@
 
 #include <boost/process/system.hpp>
 #include <boost/process/io.hpp>
+#include <boost/asio/io_service.hpp>
 
 // namespace bp = boost::process;
 
 
 std::string exec(const char* cmd)
 {
-    std::string result;
-    boost::process::system(cmd, boost::process::std_out > result);
-    return result;
+    boost::asio::io_service ios;
+    std::future<std::string> result;
+    boost::process::system(cmd, boost::process::std_out > result, ios);
+
+    std::string res = result.get();
+    return res;
 #if 0
     std::array<char, 128> buffer;
     std::string result;
