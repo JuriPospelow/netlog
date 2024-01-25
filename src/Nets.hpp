9ce5cccf88e80ebdd2509999ff780f48cb022a63
Juri <jur@pospelow.de>
2024-01-25 07:50:57 +0100

    netlog: Nets: make nets public

--- src/Nets.hpp
+++ src/Nets.hpp
@@ -14,9 +14,11 @@ public:
     void printIni() const;
     int cnt_nets() const { return _cnt_nets; }
 
+    std::vector<Net> nets{};
+
 private:
     int _cnt_nets{};
     boost::property_tree::ptree config;
-    std::vector<Net> nets{};
+
     Net create_net(int cnt_dev, const std::string& tmp_name, const std::string& );
 };
